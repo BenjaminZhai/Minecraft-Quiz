3405315d3b4a85190262e3fc8b90c7e13c65ca19
BenjaminZhai <99426763+BenjaminZhai@users.noreply.github.com>
2023-01-13 10:17:11 -0700

    asdas

--- Program.cs
+++ Program.cs
@@ -8,10 +8,11 @@ Console.WriteLine("Welcome to the amazing minecraft quiz!!!");
 int score = 0;
 
 
-// INPUT
+// INPUT Q1
 Console.WriteLine("\n1. Name a green hostile mob.");
 Console.Write("Q1 Answer: ");
 string ans1 = Console.ReadLine().ToLower();
+// Output Q1
 if (ans1 == "zombie" || ans1 == "creeper" || ans1 == "slime")
 {
     score++;
@@ -22,11 +23,12 @@ else
     Console.WriteLine("INCORRECT!");
 }
 
-
+// INPUT Q2
 Console.WriteLine("\n2. What experience level do you start at?");
 Console.Write("Q2 Answer: ");
-int ans2 = Convert.ToInt32(Console.ReadLine());
-if (ans2 == 0)
+string ans2 = Console.ReadLine();
+// Output Q2
+if (ans2 == "0")
 {
     score++;
     Console.WriteLine("CORRECT!");
@@ -36,9 +38,11 @@ else
     Console.WriteLine("INCORRECT!");
 }
 
+// INPUT Q3
 Console.WriteLine("\n3. Which material makes armour with the most protection given?");
 Console.Write("Q3 Answer: ");
 string ans3 = Console.ReadLine().ToLower();
+// Output Q3
 if (ans3 == "diamond" || ans3 == "netherite")
 {
     score++;
@@ -49,10 +53,12 @@ else
     Console.WriteLine("INCORRECT!");
 }
 
+// INPUT Q4
 Console.WriteLine("\n4. How many netherite scraps do you need for a full set of armour?");
 Console.Write("Q4 Answer: ");
-int ans4 = Convert.ToInt32(Console.ReadLine());
-if (ans4 == 20)
+string ans4 = Console.ReadLine();
+// Output Q4
+if (ans4 == "20")
 {
     score++;
     Console.WriteLine("CORRECT!");
@@ -62,7 +68,8 @@ else
     Console.WriteLine("INCORRECT!");
 }
 
-Console.WriteLine($"YOU SCORED {score}/4 !");
+// Final Score
+Console.WriteLine($"YOU SCORED {score}/4 ! ({score / 4 * 100}%)");
 if (score < 3)
 {
     Console.WriteLine("Try Again");
